93d314c29bd9f03abf1e4600323bb18a28928e06
eyiza <precious.michael2002@gmail.com>
2025-04-18 19:23:40 +0100

    added arduino test codes

--- src/msds_firmware/firmware/simple_encoder_reader/simple_encoder_reader.ino
+++ src/msds_firmware/firmware/simple_encoder_reader/simple_encoder_reader.ino
@@ -1,14 +1,16 @@
+// #include <EnableInterrupt.h>
+
 // L298N H-Bridge Connection PINs
 #define L298N_enA 9  // PWM
 #define L298N_in2 13  // Dir Motor A
 #define L298N_in1 12  // Dir Motor A
 
-#define right_encoder_phaseA 3  // Interrupt 
-#define right_encoder_phaseB 5  
+#define right_encoder_phaseA 18  // Interrupt 
+#define right_encoder_phaseB 19  
 
 unsigned int right_encoder_counter = 0;
-String right_encoder_sign = "p";
-double right_wheel_meas_vel = 0.0;    // rad/s
+String right_encoder_sign = "p"; // Direction. p means positive/clockwise direction
+double right_wheel_meas_vel = 0.0;    // Velocity vad/s
 
 void setup() {
   // Set pin modes
@@ -23,15 +25,26 @@ void setup() {
   Serial.begin(115200);
 
   pinMode(right_encoder_phaseB, INPUT);
+
+  // Set up interrupt for right encoder
   attachInterrupt(digitalPinToInterrupt(right_encoder_phaseA), rightEncoderCallback, RISING);
+  // enableInterrupt(right_encoder_phaseA, rightEncoderCallback, RISING);
 }
 
 void loop() {
   right_wheel_meas_vel = (10 * right_encoder_counter * (60.0/385.0)) * 0.10472;
+
+  // Message to be sent to the serial port
   String encoder_read = "r" + right_encoder_sign + String(right_wheel_meas_vel);
   Serial.println(encoder_read);
+
+  // Reset the counter
   right_encoder_counter = 0;
+
+  // Set the motor speed
   analogWrite(L298N_enA, 100);
+
+  // Delay for 100ms
   delay(100);
 }
 
@@ -39,11 +52,11 @@ void rightEncoderCallback()
 {
   if(digitalRead(right_encoder_phaseB) == HIGH)
   {
-    right_encoder_sign = "p";
+    right_encoder_sign = "p"; // Positive/clockwise direction
   }
   else
   {
-    right_encoder_sign = "n";
+    right_encoder_sign = "n"; // Negative/anti-clockwise direction
   }
   right_encoder_counter++;
 }
--- src/msds_firmware/firmware/simple_motor_control/simple_motor_control.ino
+++ src/msds_firmware/firmware/simple_motor_control/simple_motor_control.ino
@@ -1,27 +1,45 @@
-// L298N H-Bridge Connection PINs
-#define L298N_enA 9  // PWM
-#define L298N_in2 13  // Dir Motor A
-#define L298N_in1 12  // Dir Motor A
+// === Front Left Motor ===
+#define FL_IN1 12
+#define FL_IN2 11
+#define FL_EN  13
 
-float cmd = 0;
+// === Front Right Motor ===
+#define FR_IN1 10
+#define FR_IN2 9
+#define FR_EN  8
+
+// === Rear Left Motor ===
+#define RL_IN1 5
+#define RL_IN2 6
+#define RL_EN  7
+
+// === Rear Right Motor ===
+#define RR_IN1 22
+#define RR_IN2 23
+#define RR_EN  4
+
+// === Motor speed ===
+int motorSpeed = 150; // value between 0-255
 
 void setup() {
-  // Set pin modes
-  pinMode(L298N_enA, OUTPUT);
-  pinMode(L298N_in1, OUTPUT);
-  pinMode(L298N_in2, OUTPUT);
-  
-  // Set Motor Rotation Direction
-  digitalWrite(L298N_in1, HIGH);
-  digitalWrite(L298N_in2, LOW);
+  // Set all motor pins as outputs
+  pinMode(FL_IN1, OUTPUT); pinMode(FL_IN2, OUTPUT); pinMode(FL_EN, OUTPUT);
+  pinMode(FR_IN1, OUTPUT); pinMode(FR_IN2, OUTPUT); pinMode(FR_EN, OUTPUT);
+  pinMode(RL_IN1, OUTPUT); pinMode(RL_IN2, OUTPUT); pinMode(RL_EN, OUTPUT);
+  pinMode(RR_IN1, OUTPUT); pinMode(RR_IN2, OUTPUT); pinMode(RR_EN, OUTPUT);
 
-  Serial.begin(115200);
+  // === Move Forward ===
+  digitalWrite(FL_IN1, HIGH); digitalWrite(FL_IN2, LOW);
+  digitalWrite(FR_IN1, HIGH); digitalWrite(FR_IN2, LOW);
+  digitalWrite(RL_IN1, HIGH); digitalWrite(RL_IN2, LOW);
+  digitalWrite(RR_IN1, HIGH); digitalWrite(RR_IN2, LOW);
+
+  analogWrite(FL_EN, motorSpeed);
+  analogWrite(FR_EN, motorSpeed);
+  analogWrite(RL_EN, motorSpeed);
+  analogWrite(RR_EN, motorSpeed);
 }
 
 void loop() {
-  if (Serial.available())
-  {
-    cmd = Serial.readString().toFloat();
-  }
-  analogWrite(L298N_enA, cmd*100);
+  // Robot keeps moving forward
 }

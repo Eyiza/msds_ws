144a2a787c17a768e9b811759d9cca19849b8571
eyiza <precious.michael2002@gmail.com>
2025-04-20 19:18:16 +0100

    added comments for readability

--- src/msds_firmware/include/msds_firmware/msds_interface.hpp
+++ src/msds_firmware/include/msds_firmware/msds_interface.hpp
@@ -6,14 +6,14 @@
 // the class is only defined once, even if the header file is included multiple times
 // in different translation units.
 
-#include <rclcpp/rclcpp.hpp>
-#include <hardware_interface/system_interface.hpp>
-#include <libserial/SerialPort.h>
-#include <rclcpp_lifecycle/state.hpp>
-#include <rclcpp_lifecycle/node_interfaces/lifecycle_node_interface.hpp>
+#include <rclcpp/rclcpp.hpp> // Core ROS 2 C++ node features.
+#include <hardware_interface/system_interface.hpp> // Base class for hardware interfaces. It makes this class a hardware interface for ros2_control.
+#include <libserial/SerialPort.h> // LibSerial library for serial communication.
+#include <rclcpp_lifecycle/state.hpp> // Lifecycle state management.
+#include <rclcpp_lifecycle/node_interfaces/lifecycle_node_interface.hpp> // Lifecycle node interface.
 
-#include <vector>
-#include <string>
+#include <vector> // Standard library for using vectors which are dynamic arrays / lists.
+#include <string> // Standard library for using strings.
 
 // This header file is part of the MSDS firmware interface for ROS 2.
 // It defines the MSDSInterface class, which is responsible for communicating
@@ -25,38 +25,56 @@
 // and includes the necessary headers for working with ROS 2 lifecycle nodes
 // and hardware interfaces.
 
-namespace msds_firmware
+namespace msds_firmware // Namespace for the MSDS firmware interface
 {
     // an alias for the CallbackReturn type from the lifecycle node interface
     // This is used to simplify the code and make it more readable
     // CallbackReturn is a type that represents the return value of lifecycle
     using CallbackReturn = rclcpp_lifecycle::node_interfaces::LifecycleNodeInterface::CallbackReturn;
 
+    // Create a class named MSDSInterface that inherits from hardware_interface::SystemInterface
     class MSDSInterface : public hardware_interface::SystemInterface
     {
     public:
-        MSDSInterface(); // Constructor
-        virtual ~MSDSInterface(); //
+        MSDSInterface(); // Constructor - runs when the object is created.
+        virtual ~MSDSInterface(); // Destructor - runs when the object is deleted.
 
+        // Methods
         // Implementing rclcpp_lifecycle::node_interfaces::LifecycleNodeInterface
         // Redefining the lifecycle methods to manage the state of the node
-        CallbackReturn on_activate(const rclcpp_lifecycle::State &) override;
+        // virtual CallbackReturn on_activate(const rclcpp_lifecycle::State &previous_state) override;
+        CallbackReturn on_activate(const rclcpp_lifecycle::State &) override; // overrides the vitual method
         CallbackReturn on_deactivate(const rclcpp_lifecycle::State &) override;
 
         // Implementing hardware_interface::SystemInterface
-        CallbackReturn on_init(const hardware_interface::HardwareInfo &hardware_info) override;
-        std::vector<hardware_interface::StateInterface> export_state_interfaces() override;
-        std::vector<hardware_interface::CommandInterface> export_command_interfaces() override;
-        hardware_interface::return_type read(const rclcpp::Time &, const rclcpp::Duration &) override;
-        hardware_interface::return_type write(const rclcpp::Time &, const rclcpp::Duration &) override;
+        CallbackReturn on_init(const hardware_interface::HardwareInfo &hardware_info) override; // Called when ROS first loads your hardware.
+        std::vector<hardware_interface::StateInterface> export_state_interfaces() override; // Exports the state interfaces of the hardware.
+        std::vector<hardware_interface::CommandInterface> export_command_interfaces() override; // Exports the command interfaces of the hardware.
+
+        // Logic for reading and writing data to/from the hardware.
+        hardware_interface::return_type read(const rclcpp::Time &, const rclcpp::Duration &) override; // Reads the state of the hardware.
+        hardware_interface::return_type write(const rclcpp::Time &, const rclcpp::Duration &) override; // Writes commands to the hardware.
 
     private:
-        LibSerial::SerialPort arduino_;
-        std::string port_;
-        std::vector<double> velocity_commands_;
-        std::vector<double> position_states_;
-        std::vector<double> velocity_states_;
-        rclcpp::Time last_run_;
+        // Properties
+        LibSerial::SerialPort arduino_; // Object to communicate with your Arduino over USB.
+        std::string port_; // The port name where the Arduino is connected.
+
+        /* 
+            We have four motors (front right, front left, rear right, rear left).
+            Recall: To connect ros2 control to the hardware, we need to define the command and state interfaces for each joint.
+            The command_interface defines how we want to write messages to the hardware.
+            The state_interface defines how we want to read/recieve messages or feedback from the hardware like motors.
+            There are 3 control modes: position, velocity, and effort. 
+            In this case, we want to control the speed of the motors, so we use velocity command interfaces.
+            We also want to read the position and speed of the motors, so we use position and velocity state interfaces.
+            The command and state interfaces are stored in vectors (dynamic arrays) to allow for easy resizing.
+        */
+        std::vector<double> velocity_commands_; // Speed commands for the motors.
+        std::vector<double> position_states_; // Current position of the motors.
+        std::vector<double> velocity_states_; // Current speed of the motors.
+
+        rclcpp::Time last_run_; // The last time the read/write functions were called.
     };
 }  // namespace msds_firmware
 
--- src/msds_firmware/src/msds_interface.cpp
+++ src/msds_firmware/src/msds_interface.cpp
@@ -1,217 +1,240 @@
-#include "msds_firmware/msds_interface.hpp"
-#include <hardware_interface/types/hardware_interface_type_values.hpp>
-#include <pluginlib/class_list_macros.hpp>
+#include "msds_firmware/msds_interface.hpp" // Header file created previously for the MSDSInterface class
+#include <hardware_interface/types/hardware_interface_type_values.hpp> // For hardware interface type values
+#include <pluginlib/class_list_macros.hpp> // Lets ROS2 dynamically load this class like a plugin
 
 
 namespace msds_firmware
 {
-MSDSInterface::MSDSInterface()
-{
-}
 
-
-MSDSInterface::~MSDSInterface()
-{
-  if (arduino_.IsOpen())
+  // Constructor for the MSDSInterface class - Called when the object is first created.
+  MSDSInterface::MSDSInterface()
   {
-    try
+  }
+
+  // Destructor for the MSDSInterface class - Runs when the object is destroyed.
+  // It closes the serial connection to arduino if it is open.
+  MSDSInterface::~MSDSInterface()
+  {
+    if (arduino_.IsOpen())
     {
-      arduino_.Close();
-    }
-    catch (...)
-    {
-      RCLCPP_FATAL_STREAM(rclcpp::get_logger("MSDSInterface"),
-                          "Something went wrong while closing connection with port " << port_);
-    }
-  }
-}
-
-
-CallbackReturn MSDSInterface::on_init(const hardware_interface::HardwareInfo &hardware_info)
-{
-  CallbackReturn result = hardware_interface::SystemInterface::on_init(hardware_info);
-  if (result != CallbackReturn::SUCCESS)
-  {
-    return result;
-  }
-
-  try
-  {
-    port_ = info_.hardware_parameters.at("port");
-  }
-  catch (const std::out_of_range &e)
-  {
-    RCLCPP_FATAL(rclcpp::get_logger("MSDSInterface"), "No Serial Port provided! Aborting");
-    return CallbackReturn::FAILURE;
-  }
-
-  velocity_commands_.reserve(info_.joints.size());
-  position_states_.reserve(info_.joints.size());
-  velocity_states_.reserve(info_.joints.size());
-  last_run_ = rclcpp::Clock().now();
-
-  return CallbackReturn::SUCCESS;
-}
-
-
-std::vector<hardware_interface::StateInterface> MSDSInterface::export_state_interfaces()
-{
-  std::vector<hardware_interface::StateInterface> state_interfaces;
-
-  // Provide only a position Interafce
-  for (size_t i = 0; i < info_.joints.size(); i++)
-  {
-    state_interfaces.emplace_back(hardware_interface::StateInterface(
-        info_.joints[i].name, hardware_interface::HW_IF_POSITION, &position_states_[i]));
-    state_interfaces.emplace_back(hardware_interface::StateInterface(
-        info_.joints[i].name, hardware_interface::HW_IF_VELOCITY, &velocity_states_[i]));
-  }
-
-  return state_interfaces;
-}
-
-
-std::vector<hardware_interface::CommandInterface> MSDSInterface::export_command_interfaces()
-{
-  std::vector<hardware_interface::CommandInterface> command_interfaces;
-
-  // Provide only a velocity Interafce
-  for (size_t i = 0; i < info_.joints.size(); i++)
-  {
-    command_interfaces.emplace_back(hardware_interface::CommandInterface(
-        info_.joints[i].name, hardware_interface::HW_IF_VELOCITY, &velocity_commands_[i]));
-  }
-
-  return command_interfaces;
-}
-
-
-CallbackReturn MSDSInterface::on_activate(const rclcpp_lifecycle::State &)
-{
-  RCLCPP_INFO(rclcpp::get_logger("MSDSInterface"), "Starting robot hardware ...");
-
-  // Reset commands and states
-  velocity_commands_ = { 0.0, 0.0 };
-  position_states_ = { 0.0, 0.0 };
-  velocity_states_ = { 0.0, 0.0 };
-
-  try
-  {
-    arduino_.Open(port_);
-    arduino_.SetBaudRate(LibSerial::BaudRate::BAUD_115200);
-  }
-  catch (...)
-  {
-    RCLCPP_FATAL_STREAM(rclcpp::get_logger("MSDSInterface"),
-                        "Something went wrong while interacting with port " << port_);
-    return CallbackReturn::FAILURE;
-  }
-
-  RCLCPP_INFO(rclcpp::get_logger("MSDSInterface"),
-              "Hardware started, ready to take commands");
-  return CallbackReturn::SUCCESS;
-}
-
-
-CallbackReturn MSDSInterface::on_deactivate(const rclcpp_lifecycle::State &)
-{
-  RCLCPP_INFO(rclcpp::get_logger("MSDSInterface"), "Stopping robot hardware ...");
-
-  if (arduino_.IsOpen())
-  {
-    try
-    {
-      arduino_.Close();
-    }
-    catch (...)
-    {
-      RCLCPP_FATAL_STREAM(rclcpp::get_logger("MSDSInterface"),
-                          "Something went wrong while closing connection with port " << port_);
-    }
-  }
-
-  RCLCPP_INFO(rclcpp::get_logger("MSDSInterface"), "Hardware stopped");
-  return CallbackReturn::SUCCESS;
-}
-
-
-hardware_interface::return_type MSDSInterface::read(const rclcpp::Time &,
-                                                          const rclcpp::Duration &)
-{
-  // Interpret the string
-  if(arduino_.IsDataAvailable())
-  {
-    auto dt = (rclcpp::Clock().now() - last_run_).seconds();
-    std::string message;
-    arduino_.ReadLine(message);
-    std::stringstream ss(message);
-    std::string res;
-    int multiplier = 1;
-    while(std::getline(ss, res, ','))
-    {
-      multiplier = res.at(1) == 'p' ? 1 : -1;
-
-      if(res.at(0) == 'r')
+      try
       {
-        velocity_states_.at(0) = multiplier * std::stod(res.substr(2, res.size()));
-        position_states_.at(0) += velocity_states_.at(0) * dt;
+        arduino_.Close();
       }
-      else if(res.at(0) == 'l')
+      catch (...)
       {
-        velocity_states_.at(1) = multiplier * std::stod(res.substr(2, res.size()));
-        position_states_.at(1) += velocity_states_.at(1) * dt;
+        RCLCPP_FATAL_STREAM(rclcpp::get_logger("MSDSInterface"),
+                            "Something went wrong while closing connection with port " << port_);
       }
     }
+  }
+
+  // This function is called when the hardware interface is initialized - when ROS2 is setting up your hardware.
+  CallbackReturn MSDSInterface::on_init(const hardware_interface::HardwareInfo &hardware_info)
+  {
+    CallbackReturn result = hardware_interface::SystemInterface::on_init(hardware_info);
+    // If error
+    if (result != CallbackReturn::SUCCESS)
+    {
+      return result;
+    }
+
+    try
+    {
+      // info_ is inherited from SystemInterface
+      // Note that .hardware_parameters.at() can also be written as .hardware_parameters["port"]
+      port_ = info_.hardware_parameters.at("port"); // Get the port name from the hardware parameters
+    }
+    catch (const std::out_of_range &e)
+    {
+      RCLCPP_FATAL(rclcpp::get_logger("MSDSInterface"), "No Serial Port provided! Aborting");
+      return CallbackReturn::FAILURE;
+    }
+
+    // Reserve space for the command and state vectors to match the number of joints.
+    // This is done to avoid reallocating memory every time we add a new element
+    velocity_commands_.reserve(info_.joints.size());
+    position_states_.reserve(info_.joints.size());
+    velocity_states_.reserve(info_.joints.size());
+
     last_run_ = rclcpp::Clock().now();
+
+    return CallbackReturn::SUCCESS;
   }
-  return hardware_interface::return_type::OK;
-}
 
 
-hardware_interface::return_type MSDSInterface::write(const rclcpp::Time &,
-                                                          const rclcpp::Duration &)
-{
-  // Implement communication protocol with the Arduino
-  std::stringstream message_stream;
-  char right_wheel_sign = velocity_commands_.at(0) >= 0 ? 'p' : 'n';
-  char left_wheel_sign = velocity_commands_.at(1) >= 0 ? 'p' : 'n';
-  std::string compensate_zeros_right = "";
-  std::string compensate_zeros_left = "";
-  if(std::abs(velocity_commands_.at(0)) < 10.0)
+  // This function returns a vector (list) of state interfaces.
+  std::vector<hardware_interface::StateInterface> MSDSInterface::export_state_interfaces()
   {
-    compensate_zeros_right = "0";
-  }
-  else
-  {
-    compensate_zeros_right = "";
-  }
-  if(std::abs(velocity_commands_.at(1)) < 10.0)
-  {
-    compensate_zeros_left = "0";
-  }
-  else
-  {
-    compensate_zeros_left = "";
-  }
-  
-  message_stream << std::fixed << std::setprecision(2) << 
-    "r" << right_wheel_sign << compensate_zeros_right << std::abs(velocity_commands_.at(0)) << 
-    ",l" <<  left_wheel_sign << compensate_zeros_left << std::abs(velocity_commands_.at(1)) << ",";
+    // Vector variable to store state_intefaces
+    std::vector<hardware_interface::StateInterface> state_interfaces;
 
-  try
-  {
-    arduino_.Write(message_stream.str());
-  }
-  catch (...)
-  {
-    RCLCPP_ERROR_STREAM(rclcpp::get_logger("MSDSInterface"),
-                        "Something went wrong while sending the message "
-                            << message_stream.str() << " to the port " << port_);
-    return hardware_interface::return_type::ERROR;
+    // Provide only a position Interaface
+    for (size_t i = 0; i < info_.joints.size(); i++) // For each joint. Note that size_t is an unsigned integer type used in C++ for array indexing.
+    {
+      // Add the state interfaces to the vector
+      // emplace_back(...) is like Python’s list.append(...), but more efficient. 
+      // info_.joints[i].name → name of the joint
+      // HW_IF_POSITION → predefined constant (a string) for "position"
+      // &position_states_[i] → pointer to the memory where the current joint’s position is stored
+      state_interfaces.emplace_back(hardware_interface::StateInterface(
+          info_.joints[i].name, hardware_interface::HW_IF_POSITION, &position_states_[i]));
+      state_interfaces.emplace_back(hardware_interface::StateInterface(
+          info_.joints[i].name, hardware_interface::HW_IF_VELOCITY, &velocity_states_[i]));
+    }
+
+    return state_interfaces;
   }
 
-  return hardware_interface::return_type::OK;
-}
+
+  // returns command interfaces that ROS2 will use to send commands to your robot.
+  std::vector<hardware_interface::CommandInterface> MSDSInterface::export_command_interfaces()
+  {
+    std::vector<hardware_interface::CommandInterface> command_interfaces;
+
+    // Provide only a velocity Interaface
+    for (size_t i = 0; i < info_.joints.size(); i++)
+    {
+      command_interfaces.emplace_back(hardware_interface::CommandInterface(
+          info_.joints[i].name, hardware_interface::HW_IF_VELOCITY, &velocity_commands_[i]));
+    }
+
+    return command_interfaces;
+  }
+
+
+  // on_activate(...) is part of the lifecycle API of ROS2 hardware components. 
+  // It gets called when the controller manager starts the robot.
+  CallbackReturn MSDSInterface::on_activate(const rclcpp_lifecycle::State &)
+  {
+    RCLCPP_INFO(rclcpp::get_logger("MSDSInterface"), "Starting robot hardware ...");
+    
+    // Reset commands and states
+    velocity_commands_.resize(info_.joints.size(), 0.0);
+    position_states_.resize(info_.joints.size(), 0.0);
+    velocity_states_.resize(info_.joints.size(), 0.0);
+    // OR:
+    // velocity_commands_ = { 0.0, 0.0, 0.0, 0.0 };
+    // position_states_ = { 0.0, 0.0, 0.0, 0.0 };
+    // velocity_states_ = { 0.0, 0.0, 0.0, 0.0 };
+
+    try
+    {
+      // Opens the serial port and sets the baud rate.
+      arduino_.Open(port_);
+      arduino_.SetBaudRate(LibSerial::BaudRate::BAUD_115200);
+    }
+    catch (...) // Catches any exception
+    {
+      RCLCPP_FATAL_STREAM(rclcpp::get_logger("MSDSInterface"),
+                          "Something went wrong while interacting with port " << port_);
+      return CallbackReturn::FAILURE;
+    }
+
+    RCLCPP_INFO(rclcpp::get_logger("MSDSInterface"),
+                "Hardware started, ready to take commands");
+    return CallbackReturn::SUCCESS;
+  }
+
+  // Similar to on_activate, but the reverse.
+  CallbackReturn MSDSInterface::on_deactivate(const rclcpp_lifecycle::State &)
+  {
+    RCLCPP_INFO(rclcpp::get_logger("MSDSInterface"), "Stopping robot hardware ...");
+
+    if (arduino_.IsOpen())
+    {
+      try
+      {
+        arduino_.Close();
+      }
+      catch (...)
+      {
+        RCLCPP_FATAL_STREAM(rclcpp::get_logger("MSDSInterface"),
+                            "Something went wrong while closing connection with port " << port_);
+      }
+    }
+
+    RCLCPP_INFO(rclcpp::get_logger("MSDSInterface"), "Hardware stopped");
+    return CallbackReturn::SUCCESS;
+  }
+
+  // This is where the robot reads sensor data sent by Arduino.
+  // ROS2 calls this periodically (e.g., 100Hz) to read data from hardware.
+  hardware_interface::return_type MSDSInterface::read(const rclcpp::Time &,
+                                                            const rclcpp::Duration &)
+  {
+    // Interpret the string
+    if(arduino_.IsDataAvailable()) // Checks if there's something to read.
+    {
+      // Calculates how much time has passed since last read() 
+      // — used for integrating velocity into position.
+      auto dt = (rclcpp::Clock().now() - last_run_).seconds(); 
+
+      std::string message; 
+      arduino_.ReadLine(message); // Gets a full line from serial e.g "frp0.23,fln0.21,rrp0.25,rln0.22"
+
+      std::stringstream ss(message); // Creates a string stream from the message.
+      std::string res;
+      int multiplier = 1;
+
+      size_t index = 0;
+
+      while(std::getline(ss, res, ',') && index < velocity_states_.size()) // Splits the message into parts by comma.
+      {
+        multiplier = res.at(2) == 'p' ? 1 : -1; // Extracts the sign. res.at(2) accesses the third character ('p' or 'n').
+
+        // Convert the string after sign into a float and stores it as velocity.
+        // std::stod() = string to double 
+        // substr(3) means “slice from index 3 onward”.
+        velocity_states_.at(index) = multiplier * std::stod(res.substr(3, res.size()));
+
+        // Integrate velocity over time to update position.
+        position_states_.at(index) += velocity_states_.at(index) * dt;
+        index++;
+      }
+      // update last_run_
+      last_run_ = rclcpp::Clock().now();
+    }
+    return hardware_interface::return_type::OK;
+  }
+
+
+  // This is where we send commands to the Arduino to move the wheels.
+  hardware_interface::return_type MSDSInterface::write(const rclcpp::Time &,
+                                                            const rclcpp::Duration &)
+  {
+    if (!arduino_.IsOpen()) return hardware_interface::return_type::OK;
+
+    // Implement communication protocol with the Arduino
+    std::stringstream message_stream;
+    const std::vector<std::string> wheel_labels = {"fr", "fl", "br", "bl"};
+
+    for (size_t i = 0; i < velocity_commands_.size(); ++i) {
+      char sign = velocity_commands_.at(i) >= 0 ? 'p' : 'n';
+      double speed = std::abs(velocity_commands_.at(i));
+      std::string pad_zero = (speed < 10.0) ? "0" : ""; // pad_zero is used to add a leading zero if speed is less than 10.0
+
+      // Prepares the message string to be sent to Arduino
+      // std::fixed + std::setprecision(2) makes sure we get two decimal places.
+      message_stream << wheel_labels[i] << sign << pad_zero << std::fixed << std::setprecision(2) << speed << ",";
+    }
+
+    try
+    {
+      arduino_.Write(message_stream.str()); // Sends the message string to arduino
+    }
+    catch (...)
+    {
+      RCLCPP_ERROR_STREAM(rclcpp::get_logger("MSDSInterface"),
+                          "Something went wrong while sending the message "
+                              << message_stream.str() << " to the port " << port_);
+      return hardware_interface::return_type::ERROR;
+    }
+
+    return hardware_interface::return_type::OK;
+  }
 }  // namespace msds_firmware
 
+// This is a macro that registers your class as a ROS2 hardware plugin.
+// Without it, ROS2 won't know how to load your class from the .so library during runtime.
 PLUGINLIB_EXPORT_CLASS(msds_firmware::MSDSInterface, hardware_interface::SystemInterface)

7f3b52621ad5a7d6c06824c28f338fbd2cc94094
eyiza <precious.michael2002@gmail.com>
2025-04-22 21:28:42 +0100

    updated imu driver

--- src/msds_firmware/msds_firmware/mpu6050_driver.py
+++ src/msds_firmware/msds_firmware/mpu6050_driver.py
@@ -1,11 +1,14 @@
 #!/usr/bin/env python3
 import rclpy.time
-import smbus
+import smbus # I2C communication - Used to talk to the MPU6050 sensor over I²C.
 import rclpy
 from rclpy.node import Node
 from rclpy.qos import qos_profile_sensor_data
 from sensor_msgs.msg import Imu
 
+# MPU6050 Registers Addresses
+# These values are extracted from the datasheet of the sensor, 
+# and can be used to send commmands to the board and to receive its readings.
 PWR_MGMT_1   = 0x6B
 SMPLRT_DIV   = 0x19
 CONFIG       = 0x1A
@@ -25,19 +28,21 @@ class MPU6050_Driver(Node):
     def __init__(self):
         super().__init__("mpu6050_driver")
         
-        # I2C Interafce
-        self.is_connected_ = False
-        self.init_i2c()
+        # I2C Interafce - communication with MPU6050 sensor
+        self.is_connected_ = False # Flag to check if the device is connected
+        self.init_i2c() # Initialize I2C bus
 
         # ROS 2 Interface
         self.imu_pub_ = self.create_publisher(Imu, "/imu/out", qos_profile=qos_profile_sensor_data)
-        self.imu_msg_ = Imu()
+        self.imu_msg_ = Imu() # Initialize IMU message
         self.imu_msg_.header.frame_id = "base_footprint"
-        self.frequency_ = 0.01
+
+        self.frequency_ = 0.01 # 100 Hz / 0.01 seconds i.e 100 times per second
         self.timer_ = self.create_timer(self.frequency_, self.timerCallback)
 
     def timerCallback(self):
         try:
+            # Reconnects if I²C was lost earlier
             if not self.is_connected_:
                 self.init_i2c()
             
@@ -51,10 +56,11 @@ class MPU6050_Driver(Node):
             gyro_y = self.read_raw_data(GYRO_YOUT_H)
             gyro_z = self.read_raw_data(GYRO_ZOUT_H)
             
-            # Full scale range +/- 250 degree/C as per sensitivity scale factor     
+            # Full scale range +/- 250 degree/C as per sensitivity scale factor on sensor datasheet     
             self.imu_msg_.linear_acceleration.x = acc_x / 1670.13
             self.imu_msg_.linear_acceleration.y = acc_y / 1670.13
             self.imu_msg_.linear_acceleration.z = acc_z / 1670.13
+
             self.imu_msg_.angular_velocity.x = gyro_x / 7509.55
             self.imu_msg_.angular_velocity.y = gyro_y / 7509.55
             self.imu_msg_.angular_velocity.z = gyro_z / 7509.55
@@ -77,14 +83,14 @@ class MPU6050_Driver(Node):
             self.is_connected_ = False
         
     def read_raw_data(self, addr):
-        #Accelero and Gyro value are 16-bit
+        # Accelero and Gyro value are 16-bit
         high = self.bus_.read_byte_data(DEVICE_ADDRESS, addr)
         low = self.bus_.read_byte_data(DEVICE_ADDRESS, addr+1)
         
-        #concatenate higher and lower value
+        # Concatenate higher and lower value
         value = ((high << 8) | low)
             
-        #to get signed value from mpu6050
+        # To get signed value from mpu6050
         if(value > 32768):
             value = value - 65536
         return value

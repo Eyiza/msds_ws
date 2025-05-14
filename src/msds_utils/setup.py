f44ae7b33c5721a2fd735fbff119040ca4297195
eyiza <precious.michael2002@gmail.com>
2025-05-14 16:49:44 +0100

    updated bringup and utils

--- src/msds_utils/setup.py
+++ src/msds_utils/setup.py
@@ -1,11 +1,11 @@
-from setuptools import setup
+from setuptools import find_packages, setup
 
 package_name = 'msds_utils'
 
 setup(
     name=package_name,
     version='0.0.0',
-    packages=[package_name],
+    packages=find_packages(exclude=['test']),
     data_files=[
         ('share/ament_index/resource_index/packages',
             ['resource/' + package_name]),
@@ -13,10 +13,10 @@ setup(
     ],
     install_requires=['setuptools'],
     zip_safe=True,
-    maintainer='user',
-    maintainer_email='antonio.brandi@outlook.it',
-    description='ROS 2 Code Examples',
-    license='Apache 2.0',
+    maintainer='eyiza',
+    maintainer_email='precious.michael2002@gmail.com',
+    description='TODO: Package description',
+    license='TODO: License declaration',
     tests_require=['pytest'],
     entry_points={
         'console_scripts': [
